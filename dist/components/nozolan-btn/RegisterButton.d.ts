db182efe9ff1eeedadfbdd65eb1b45b4dac03b92
AhmedSalah172002 <ahmed172002tayel@gmail.com>
2023-12-27 12:18:54 +0200

    nozolan-lib

---
+++ dist/components/nozolan-btn/RegisterButton.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { ButtonProps } from '@mui/material/Button';
+declare function RegisterButton<C extends React.ElementType>(props: ButtonProps<C, {
+    component?: C;
+}>): import("react/jsx-runtime").JSX.Element;
+export default RegisterButton;
